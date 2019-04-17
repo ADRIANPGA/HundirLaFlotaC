a7e18f613332cbf375229a6fad8824385623176d
Adrián <47591697+AdrianPerezGarcia@users.noreply.github.com>
2019-04-17 12:28:04 +0200

    Add files via upload
    Commit inicial para probar commits en Windows

---
+++ HundirLaFlota.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void desplegarCabecera();
+
+void desplegarCabecera{
+	printf("---------------------\n");
+	printf("|  Hundir la Flota  |\n");
+	printf("---------------------\n");
+	printf("\t\t\t Adrián Pérez\n");
+	printf("\n");
+}
+
+int main(int argc, char const *argv[]){
+	desplegarCabecera();
+	do{
+
+	}while(opcion != 3);
+	return 0;
+}
